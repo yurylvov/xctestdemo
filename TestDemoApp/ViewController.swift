e119478370c962c39f2dcc5534372e7394e2ed79
リヴォーフ・ユーリ <yury.lvov@pc-1748.local>
2018-10-29 14:43:25 +0900

    Initial Commit

---
+++ TestDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemoApp
+//
+//  Created by リヴォーフ　ユーリ on 2018/10/29.
+//  Copyright © 2018 リヴォーフ　ユーリ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
